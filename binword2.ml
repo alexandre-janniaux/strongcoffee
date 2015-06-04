40688965a450f7c875bd2bd7ecf962a77760d72b
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2015-06-04 20:02:11 +0200

    Mise a jour code + presentation : utilisation d'entier pour représenter minterme, correction algo,  beamer presque fini.

--- binword2.ml
+++ binword2.ml
@@ -1,20 +1,46 @@
+open Printf;;
 
 type binword = int*int (* value * mask *)
 
-let hamming_distance a b =
-    let b = ref 1 
-    and c = a lxor b in
-    let rec aux c n = 
-      if c=0 then n 
-      else if c land 1 then aux (c lsr 1) (n+1)
-      else aux (c lsr 1) n
-    in aux c 0
+let hamming_distance (a,m1) (b,m2) =
+  let c = a  lxor b in
+  let m = m1 lxor m2 in
+  let rec aux c m n = 
+    if c=0 then n 
+    else if c land 1 <> 0 || m land 1 <> 0 then aux (c lsr 1) (m lsr 1) (n+1)
+    else aux (c lsr 1) (m lsr 1) n
+  in aux c m 0
 ;;
 
 let merge_word a b = 
   let (n1, m1) = a and (n2, m2) = b in
   if m1 <> m2 then failwith "Pas les mêmes masques"
   else (n1 land m1, (n1 lxor n2) lor m1)
+;;
+
+let minterm_to_binword l nb_var =
+  let m = ((-1) lsl nb_var) in
+  let rec aux l accu = match l with
+    | x::r -> aux r ((x,m)::accu) (* -1 = lnot 0 *)
+    | _ -> accu
+  in aux l []
+;;
+
+let print_minterm (x,m) nb_var =
+  let checker = 1 lsl (nb_var -1) in
+  let rec aux x m n =
+    if n > nb_var then ()
+    else match (x,m) with
+    | 0, 0 -> ()
+    | _, _ ->
+      if m land checker = 0 then begin 
+	print_int ((x land checker) lsr (nb_var-1));
+	aux (x lsl 1) (m lsl 1) (n+1)
+      end
+      else begin print_char '_';
+	aux (x lsl 1) (m lsl 1) (n+1)
+      end
+  in aux x m 1
 ;;
 
 
--- quine3.ml
+++ quine3.ml
@@ -1,31 +1,32 @@
-#use "binword.ml";;
+#use "binword2.ml";;
 
 let quine accepted_words =
 
   let rec merge w words merged used =
     match words with
     | x::r ->
-      if hamming_distance w x (<>) = 1 then
+      if hamming_distance w x = 1 then
 	merge w r ((merge_word w x)::merged) (x::used)
       else
 	merge w r merged used
     | _ -> merged, used
   in
 
-  let rec step words accu matched unmatched = match words with
+  let rec step words accu matched prime = match words with
     | x::r ->
-      let (merged, used) = merge x words [] [] in
-      if merged <> [] then 
-	step r (merged@accu) (used@matched) unmatched
+      let (merged, used) = merge x r [] [] in
+      if merged <> [] then ()
+	step r (merged@accu) (used@matched) prime
       else
-	step r accu matched (x::unmatched)
-    | _ -> accu, matched, unmatched
+	step r accu matched (x::prime)
+    | _ -> accu, matched, prime
   in
 
   let rec aux words accu =
-    let new_words, matched, unmatched = step words [] [] [] in
-    if matched <> [] then
-      aux new_words (unmatched@accu)
+    if words <> [] then
+      let new_words, matched, prime = step words [] [] [] in
+      (* supprimer les doublons avant ? *)
+      aux new_words (prime@accu)
     else
       accu
 
